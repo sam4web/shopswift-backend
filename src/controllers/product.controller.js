32be359bf6693f2d76d0eb1b81a2b6e1cbc29f22
Sijal Manandhar <sijal.m06@gmail.com>
2024-11-20 16:11:47 +0545

    update product controller & route added, enums added on category

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,6 +3,8 @@ const base64Encode = require("../utils/base64-encode.util");
 const { isValidObjectId } = require("mongoose");
 
 
+// @route /api/products
+// @method GET
 const getAllProducts = async (req, res) => {
   const products = await Product.find({}).lean();
   if (!products.length)
@@ -11,6 +13,8 @@ const getAllProducts = async (req, res) => {
 };
 
 
+// @route /api/products/:id
+// @method GET
 const getSingleProduct = async (req, res) => {
   const id = req.params.id;
   if (!isValidObjectId(id))
@@ -22,12 +26,15 @@ const getSingleProduct = async (req, res) => {
 };
 
 
+// @route /api/products
+// @method POST
 const createProduct = async (req, res) => {
   let { name, price, description, category } = req.body;
   const imageFile = req.files.image;
 
-  name = name.trim();
-  description = description.trim();
+  name = name?.trim();
+  description = description?.trim();
+  category = category?.trim().toLowerCase();
   price = Number(price);
 
   if (isNaN(price) || price < 0)
@@ -40,11 +47,61 @@ const createProduct = async (req, res) => {
     name: imageFile.name,
     data: base64Encode(imageFile.data),
   };
-  await Product.create({ image, name, price, description, category });
-  return res.json({ message: "New Product Created." });
+
+  try {
+    const product = await Product.create({ image, name, price, description, category });
+    return res.json(product);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 };
 
 
+// @route /api/products/:id
+// @method PATCH
+const updateProduct = async (req, res) => {
+  const id = req.params.id;
+  const imageFile = req.files.image;
+  let { name, price, description, category } = req.body;
+
+  name = name?.trim();
+  description = description?.trim();
+  category = category?.trim().toLowerCase();
+  price = Number(price);
+
+  if (!isValidObjectId(id))
+    return res.status(400).send({ message: "Invalid ID provided. Please check and try again." });
+
+  if (isNaN(price) || price < 0)
+    return res.status(400).json({ "message": "Please enter a valid price number." });
+
+  if (![name, price, category].every(Boolean))
+    return res.status(400).json({ "message": "All fields are required. Please fill all fields." });
+
+  const product = await Product.findById(id).select("-__v").lean();
+  if (!product)
+    return res.status(404).send({ message: "No product found." });
+
+  const image = {
+    name: imageFile.name,
+    data: base64Encode(imageFile.data),
+  };
+
+  try {
+    const updatedProduct = await Product.findOneAndUpdate(
+      { _id: id },
+      { image, name, price, description, category },
+      { new: true },
+    );
+    return res.json(updatedProduct);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+
+// @route /api/products/:id
+// @method DELETE
 const deleteProduct = async (req, res) => {
   const id = req.params.id;
   if (!isValidObjectId(id))
@@ -59,5 +116,6 @@ module.exports = {
   getAllProducts,
   getSingleProduct,
   createProduct,
+  updateProduct,
   deleteProduct,
 };
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -2,7 +2,13 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const router = express.Router();
 
-const { getAllProducts, createProduct, getSingleProduct, deleteProduct } = require("../controllers/product.controller");
+const {
+  getAllProducts,
+  createProduct,
+  getSingleProduct,
+  deleteProduct,
+  updateProduct,
+} = require("../controllers/product.controller");
 const { fileExists } = require("../middlewares/file-exists.middleware");
 const { fileExtension } = require("../middlewares/file-extension.middleware");
 const { fileSize } = require("../middlewares/file-size.middleware");
@@ -19,6 +25,13 @@ router.route("/")
 
 router.route("/:id")
   .get(getSingleProduct)
-  .delete(deleteProduct);
+  .delete(deleteProduct)
+  .patch(
+    fileUpload({ createParentPath: true }),
+    fileExists,
+    fileExtension([".jpg", ".jpeg", ".png"]),
+    fileSize,
+    updateProduct,
+  );
 
 module.exports = router;
